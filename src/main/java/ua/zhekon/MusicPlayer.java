5199383ae1504689e0676dc7052453c4e975a312
zhekon19 <zhekon19@gmail.com>
2021-10-06 20:32:55 +0300

    add resouces

--- src/main/java/ua/zhekon/MusicPlayer.java
+++ src/main/java/ua/zhekon/MusicPlayer.java
@@ -1,12 +1,40 @@
 package ua.zhekon;
 
 public class MusicPlayer {
-    Music music;
+    private Music music;
+    private String name;
+    private int volume;
+
+
+    public MusicPlayer() {
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
-    public void playMusic(){
+
+    public void setMusic(Music music) {
+        this.music = music;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void playMusic() {
         System.out.println(music.getSong());
     }
 }
--- src/main/java/ua/zhekon/TestSpring.java
+++ src/main/java/ua/zhekon/TestSpring.java
@@ -6,9 +6,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean",Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("musicBean",Music.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayerBean",MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume()+20);
         context.close();
     }
 }
